e765ae8a664d9ec829239b1e66852567cc9c1e19
Alexsey <107761962+CyberZaran@users.noreply.github.com>
2022-06-23 12:40:41 +0300

    Add files via upload

---
+++ OOP_4.py
@@ -0,0 +1,26 @@
+class Building:
+	year = None
+	__city = None # Инкапсуляция, но в питоне она плохо реализована.
+
+	def __init__(self, year, city):
+		self.year = year
+		self.city = city
+
+	def get_info(self):
+		print("Year:", self.year, "City:", self.city)
+
+# Класс наследник.
+class School(Building):
+	pupils = 0
+
+	def __init__(self, pupils, year, city):
+		super(School, self).__init__(year,city)
+		self.pupils = pupils
+
+# Полиморфизм.
+	def get_info(self):
+		super().get_info()
+		print("Pupils:", self.pupils)
+
+school = School(100, 2000, "Moscow")
+school.get_info()
